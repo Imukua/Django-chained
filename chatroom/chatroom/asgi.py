350d43767f81c78f94a1ed58124664f519143ee9
Imukua <ianmukuaa@gmail.com>
2023-11-01 13:47:31 +0300

    added websocket

--- chatroom/chatroom/asgi.py
+++ chatroom/chatroom/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter (
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
